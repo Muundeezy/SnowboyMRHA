53805928fe4b06a9db05d925af8a93359d87ce05
Hanzala Ibn Zahid <35690425+hanzala123@users.noreply.github.com>
2021-04-26 22:08:16 +0600

    add support for using more than 3 wav file to train a model

--- examples/Python/generate_pmdl.py
+++ examples/Python/generate_pmdl.py
@@ -5,6 +5,7 @@ import uuid
 from scipy.io import wavfile
 from pmdl import snowboy_pmdl_config
 from pmdl.snowboy_pmdl import SnowboyPersonalEnroll, SnowboyTemplateCut
+from glob import glob
 
 def check_enroll_output(enroll_ans):
     if enroll_ans == -1:
@@ -17,9 +18,9 @@ def check_enroll_output(enroll_ans):
 
 def main():
     parser = argparse.ArgumentParser(description='Command line client for generating snowboy personal model')
-    parser.add_argument('-r1', '--record1', dest="record1", required=True, help="Record voice 1")
-    parser.add_argument('-r2', '--record2', dest="record2", required=True, help="Record voice 2")
-    parser.add_argument('-r3', '--record3', dest="record3", required=True, help="Record voice 3")
+    parser.add_argument('-r1', '--record1', dest="record1", required=False, help="Record voice 1")
+    parser.add_argument('-r2', '--record2', dest="record2", required=False, help="Record voice 2")
+    parser.add_argument('-r3', '--record3', dest="record3", required=False, help="Record voice 3")
     parser.add_argument('-n', '--name', dest="model_name", required=True, help="Personal model name")
     parser.add_argument('-lang', '--language', default="en", dest="language", help="Language")
     args = parser.parse_args()
@@ -41,21 +42,24 @@ def main():
     assert cut.BitsPerSample() == enroll.BitsPerSample()
     print("channels: %d, sample rate: %d, bits: %d" % (cut.NumChannels(), cut.SampleRate(), cut.BitsPerSample()))
 
-    recording_set = [args.record1, args.record2, args.record3]
-    for rec in recording_set:
-        print("processing %s" % rec)
-        _, data = wavfile.read(rec)
-        data_cut = cut.CutTemplate(data.tobytes())
-        enroll_ans = enroll.RunEnrollment(data_cut)
+    recording_set = glob('model/*.wav')
+    if recording_set:
+        for rec in recording_set:
+            print("processing %s" % rec)
+            _, data = wavfile.read(rec)
+            data_cut = cut.CutTemplate(data.tobytes())
+            enroll_ans = enroll.RunEnrollment(data_cut)
 
-    check_enroll_output(enroll_ans)
+        check_enroll_output(enroll_ans)
 
-    filename = args.model_name
-    print("saving file to %s" % filename)
-    f = open(filename, "wb")
-    f.write(open(out.name).read())
-    f.close()
-    print("finished")
+        filename = args.model_name
+        print("saving file to %s" % filename)
+        f = open(filename, "wb")
+        f.write(open(out.name).read())
+        f.close()
+        print("finished")
+    else:
+        print('No wav files in model directory')
 
 if __name__ == "__main__":
     main()
